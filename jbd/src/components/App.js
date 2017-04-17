c7e99b939580d3a306ad4b3e97de7025e7dd22aa
Erik Jacobsen <slowerthanyou@gmail.com>
2017-04-16 18:19:33 -0700

    adding header and footer

--- jbd/src/components/App.js
+++ jbd/src/components/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default React.createClass({
   render() {
     return (
+    <MuiThemeProvider>
       <h1>Hello world</h1>
+    </MuiThemeProvider>
     )
   }
 })
